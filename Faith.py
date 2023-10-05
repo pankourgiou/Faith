4b88ec9c520d3c007b841fc4c44396fa49ee1893
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-10-05 11:19:32 +0300

    Create Faith.py

---
+++ Faith.py
@@ -0,0 +1,4 @@
+#Today a big amount of toxic waste from Fukushima is going to be spoiled into the waters...
+#that's not good news for humanity...seems we must learn everyday to cope with this damage earth is facing with many different forms...a higher power tries to show us it's impossible to do anything if it's against it's Will. 
+#So we need to move to a more faithfull lifestyle and be more humble. This power can lead us to a better future and will eventually lead us to a better future.
+#we need faith.
